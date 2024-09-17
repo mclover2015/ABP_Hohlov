6f12b3bdc445a7e41b9a705952cf83ab49966628
kiril <flizarrq@gmail.com>
2024-09-17 12:31:33 +0300

    change smth

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <string>
-#include <iomanip>
 #include <windows.h>
+
+
 #include "Lab_1//Lab_1.cpp"
 #include "Lab_2//Lab_2.cpp"
 #include "Lab_3//Lab_3.cpp"
+
 using namespace std;
 
 
@@ -12,27 +13,21 @@ int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     int nomerLabaratornoi;
-    anchor:
-        cout<< "Виберіть номер лаби:"<< endl;
+    cout<< "Виберіть номер лаби:"<< endl;
     cin>> nomerLabaratornoi;
     switch(nomerLabaratornoi) {
         case 1:
             Lab_1();
-        break;
+            break;
         case 2:
             Lab_2();
-        break;
+            break;
         case 3:
             Lab_3();
-        break;
-        case 4:
-            cout<< "Введіть номер лаби:"<<endl;
-        break;
-        case 5 :
-            return 0;
+            break;
         default :
             cout<< "Неправильне значення"<< endl;
     }
-    goto anchor;
+    return 0;
 }
 
