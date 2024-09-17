6f082d49c1a5e856b2b65161d6475ae0cbb62dc2
kiril <flizarrq@gmail.com>
2024-09-17 12:13:03 +0300

    init commit

---
+++ Lab_3/Lab_3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+
+int Lab_3(){
+    long  k2 = 1;
+    double dbln, sum=0, term;
+    const double EPS = 0.000001;
+    short k1= 1;
+    int n;
+
+    for (n = 0; ;n++, k2 *=2, k1 = -1*k1) {
+        dbln = static_cast<double>(n);
+        term = k1 * (1 - ((dbln+1)*(dbln +1))/((dbln+2)*(dbln+2)));
+        if (fabs(term)>=EPS){
+            sum +=term;
+        }
+        else {
+            break;
+        }
+        if (n==9){
+            cout<<"Сума 10 членів ряду : " << fixed<< setprecision(6)<< sum<<endl;
+        }
+    }
+    cout<<"Повна сума ряду: "<< fixed<<setprecision(6)<< sum<<endl;
+    cout<<"Ітерація: "<< n << endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <windows.h>
+#include "Lab_1//Lab_1.cpp"
+#include "Lab_2//Lab_2.cpp"
+#include "Lab_3//Lab_3.cpp"
+using namespace std;
+
+
+int main(){
+
+    SetConsoleOutputCP(CP_UTF8);
+    int nomerLabaratornoi;
+    anchor:
+        cout<< "Виберіть номер лаби:"<< endl;
+    cin>> nomerLabaratornoi;
+    switch(nomerLabaratornoi) {
+        case 1:
+            Lab_1();
+        break;
+        case 2:
+            Lab_2();
+        break;
+        case 3:
+            Lab_3();
+        break;
+        case 4:
+            cout<< "Введіть номер лаби:"<<endl;
+        break;
+        case 5 :
+            return 0;
+        default :
+            cout<< "Неправильне значення"<< endl;
+    }
+    goto anchor;
+}
+
